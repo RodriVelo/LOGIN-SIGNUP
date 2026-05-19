2ee5a7ec2c3c4634bea5117ec86ada7fefda8fd0
RodriVelo <150716061+RodriVelo@users.noreply.github.com>
2026-05-19 19:17:05 -0300

    a ver

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -45,7 +45,7 @@ router.get(
   "/google/callback",
   passport.authenticate("google", {
     session: false,
-    failureRedirect: "http://localhost:5173/login",
+    failureRedirect: `${process.env.CLIENT_URL}/login`,,
   }),
   googleCallback,
 );
