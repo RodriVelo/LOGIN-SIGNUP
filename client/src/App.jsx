7be927bbfd347830f82b545f54b7c6b36457b70f
RodriVelo <150716061+RodriVelo@users.noreply.github.com>
2026-06-19 16:23:29 -0300

    jejeje

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -73,7 +73,7 @@ function App() {
           <Route
             path="/misreservas"
             element={
-              <ProtectedRoute>
+              <ProtectedRoute rolPermitido="cliente">
                 <MisReservas />
               </ProtectedRoute>
             }
